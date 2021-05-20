35e3a39c7bf91d5e4f437c33c24ccf80c5ab190c
Garri Adrian Nablo <garri.almighty@gmail.com>
2021-05-20 21:47:32 +0800

    Initial Commit

---
+++ CartrackChallenge/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CartrackChallenge
+//
+//  Created by Garri Adrian Nablo on 5/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
